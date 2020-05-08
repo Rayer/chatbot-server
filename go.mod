7d9ba1c0df0cd1908b06c8883e8a2d6d058ef7f0
Killercat <rayershih@gmail.com>
2020-05-09 06:45:25 +0800

    Add CORS support

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.13
 //replace github.com/rayer/chatbot v0.5.0 => ../ChatBot
 
 require (
+	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.6.3
 	github.com/moogar0880/problems v0.1.1
 	github.com/rayer/chatbot v0.5.0
--- server/server_main.go
+++ server/server_main.go
@@ -2,14 +2,28 @@ package main
 
 import (
 	apiLib "UtilAPIs"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/moogar0880/problems"
 	_ "github.com/rayer/chatbot"
 	"net/http"
+	"time"
 )
 
 func main() {
 	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "http://node.rayer.idv.tw"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
+
 	chatbot := apiLib.NewChatbotContext()
 
 	r.NoRoute(func(c *gin.Context) {
